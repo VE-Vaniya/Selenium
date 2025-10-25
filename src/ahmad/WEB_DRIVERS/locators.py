96cee1007848cf23f92e1e1b6d78288152230bc6
محمد احمد <muhammad.ahmad.github@gmail.com>
2025-10-26 00:27:31 +0500

    feat(basics): ft. 01

---
+++ src/ahmad/WEB_DRIVERS/locators.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from dotenv import load_dotenv
+import os
+import time
+from selenium.webdriver.common.by import By
+
+load_dotenv()
+
+#instanciate the driver
+driver = webdriver.Firefox()
+
+#open a url
+driver.get(os.getenv("WEB_DEMO"))
+driver.maximize_window()
+
+time.sleep(3)
+
+driver.find_element(By.ID, "user-name").send_keys("standard_user")
+time.sleep(1)
+
+password = driver.find_element(By.XPATH, "//input[@id='password']")
+# password = driver.find_element(By.XPATH, "//input[contains(@id,'password')]")
+# password = driver.find_element(By.XPATH, "//input[starts-with(@id,'pass')]")
+# password = driver.find_element(By.XPATH, "//input[ends-with(@id,'word')]")
+# product = driver.find_element(By.XPATH, "//div[text()='Sauce Labs Backpack']")
+# product = driver.find_element(By.XPATH, "//div[contains(text(),'Backpack')]")
+password.send_keys("secret_sauce")
+time.sleep(1)
+
+driver.find_element(By.CLASS_NAME, "btn_action").click()
+time.sleep(3)
+
+driver.quit()
