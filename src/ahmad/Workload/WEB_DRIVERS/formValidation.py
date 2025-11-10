802108a137ef0f70e25875ec6b73ed6f1815793f
محمد احمد <muhammad.ahmad.github@gmail.com>
2025-11-10 13:36:04 +0500

    feat: form validation

---
+++ src/ahmad/Workload/WEB_DRIVERS/formValidation.py
@@ -0,0 +1,83 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+import time
+from dotenv import load_dotenv
+from selenium.webdriver.support.ui import Select
+import os
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+
+load_dotenv()
+
+#Full Name
+# email
+# campus single dropdown
+# password
+# confirm password
+# image for upload (optional)
+driver.get(os.getenv("WEB_REGISTER"))
+
+driver.maximize_window()
+driver.implicitly_wait(20)
+
+
+FULLNAME=driver.find_element(By.NAME,"fullName")
+EMAIL = driver.find_element(By.NAME,"email")
+CAMPUSES = Select(driver.find_element(By.NAME,"campusId"))
+PASSWORD = driver.find_element(By.NAME,"password")
+CONFIRM_PASSWORD = driver.find_element(By.XPATH,"//input[@placeholder='Confirm your password']")
+# FILE=driver.find_element(By.XPATH,"//span[text()='browse']")
+FILE=driver.find_element(By.XPATH,"//input[@type='file']")
+SUBMIT = driver = driver.find_element(By.XPATH,"//button[@type='submit']")
+
+# all valid inputs
+# FULLNAME.send_keys("Muhammad Ahmad Butt")
+# time.sleep(1)
+
+# EMAIL.send_keys("l233059@lhr.nu.edu.pk")
+# time.sleep(1)
+
+# CAMPUSES.select_by_visible_text("Lahore") #lahore
+# time.sleep(1)
+
+# PASSWORD.send_keys("Hello123@")
+# time.sleep(1)
+
+# CONFIRM_PASSWORD.send_keys("Hello123@")
+# time.sleep(1)
+
+# base_path = os.getenv("IMG_PATH") 
+# full_path = os.path.join(base_path, "img.jpeg")
+# FILE.send_keys(full_path)
+
+# time.sleep(10)
+
+# password mismatch
+# all valid inputs
+FULLNAME.send_keys("Muhammad Ahmad Butt")
+time.sleep(1)
+
+EMAIL.send_keys("l233059@lhr.nu.edu.pk")
+time.sleep(1)
+
+CAMPUSES.select_by_visible_text("Lahore") #lahore
+time.sleep(1)
+
+PASSWORD.send_keys("Hello123@")
+time.sleep(1)
+
+CONFIRM_PASSWORD.send_keys("Hello123!")
+time.sleep(1)
+
+base_path = os.getenv("IMG_PATH") 
+full_path = os.path.join(base_path, "img.jpeg")
+FILE.send_keys(full_path)
+time.sleep(1)
+
+SUBMIT.click()
+time.sleep(10)
+
+driver.close()
