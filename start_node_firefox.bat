174ff64a65eeb90d58b9264a385620c1c6eb3756
محمد احمد <muhammad.ahmad.github@gmail.com>
2025-11-27 19:12:09 +0500

    feat: remove some files

--- start_node_firefox.bat
+++
@@ -1,5 +0,0 @@
-java -Dwebdriver.gecko.driver=C:\WebDriver\bin\geckodriver.exe ^
--jar D:\Selenium_Grid\selenium-server-4.38.0.jar node ^
---port 5557 ^
---publish-events tcp://192.168.18.38:4442 ^
---subscribe-events tcp://192.168.18.38:4443
